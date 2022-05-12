cc14f456f3cd234cac185c9204fff9e740885fff
evro77777 <90ipatov90@gmail.com>
2022-05-12 16:58:28 +0300

    12/05/22

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,15 +1,22 @@
-from rest_framework import generics, mixins
+from rest_framework import generics, mixins, permissions, authentication
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from django.http import Http404
 from .models import Product
 from .serializers import ProductSerializer
+from .permissions import IsStaffEditorPermission
+from api.authentication import TokenAuthentication
 
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    authentication_classes = [
+        authentication.SessionAuthentication,
+        TokenAuthentication
+    ]
+    permission_classes = [permissions.IsAdminUser, IsStaffEditorPermission]
 
     def perform_create(self, serializer):
         title = serializer.validated_data.get('title')
@@ -17,6 +24,8 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
         if content is None:
             content = title
         serializer.save(content=content)
+
+
 
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
@@ -44,20 +53,33 @@ class ProductDeleteAPIView(generics.DestroyAPIView):
         super().perform_destroy(instance)
 
 
-class ProductListAPIView(generics.ListAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-
-
 class ProductMixinView(
+    mixins.CreateModelMixin,
     mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
     generics.GenericAPIView
 ):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def perform_create(self, serializer):
+        title = serializer.validated_data.get("title")
+        content = serializer.validated_data.get("content")
+        if content is None:
+            content = f'{title}_content'
+        serializer.save(content=content
+
+                        )
+
+    def get(self, request, *args, **kwargs):
+        pk = kwargs.get("pk")
+        if pk is not None:
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
+        return self.create(request, *args, **kwargs)
 
 
 @api_view(['GET', 'POST'])
@@ -85,6 +107,6 @@ def product_alt_view(request, pk=None, *args, **kwargs):
 
 product_detail_view = ProductDetailAPIView.as_view()
 product_list_create_view = ProductListCreateAPIView.as_view()
-product_list_view = ProductListAPIView.as_view()
 product_update_view = ProductUpdateAPIView.as_view()
 product_delete_view = ProductDeleteAPIView.as_view()
+product_mixin_view = ProductMixinView.as_view()
--- py_client/create.py
+++ py_client/create.py
@@ -4,7 +4,7 @@ endpoint = 'http://127.0.0.1:8000/api/products/'
 
 
 data = {
-    "title": "this field is done",
+    "title": "mixin_product",
 }
 get_response = requests.post(endpoint,json=data)
 print(get_response.json())
